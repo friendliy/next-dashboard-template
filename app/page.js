9fa950c16a2884d530db44424664e70178df63b5
Alizha <105841106722@student.unismuh.ac.id>
2024-08-15 00:07:51 -0700

    moodivikasi chapter 6 selesai

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-import AcmeLogo from "@/app/ui/acme-logo";
+// /app/page.jsx
+import AcmeLogo from "./ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+    <main className="flex flex-col min-h-screen p-6">
+      <div className="flex items-end p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
         <AcmeLogo />
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
@@ -20,25 +20,31 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base"
+            className="flex items-center self-start gap-3 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
-            <ArrowRightIcon className="w-5 md:w-6"></ArrowRightIcon>
+            <ArrowRightIcon className="w-5 h-5" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image
-            src="/hero-Mobile.png"
-            width={560}
-           
-            height={620}
+          <img
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
+          <img
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       </div>
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black mt-4" />
     </main>
   );
 }
