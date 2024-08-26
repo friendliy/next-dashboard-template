a47d6d1e4816516fade2d3fa2a252cba8fae878a
Alizha <105841106722@student.unismuh.ac.id>
2024-08-26 02:35:51 -0700

    chapter 9 selesai

--- app/ui/dashboard/revenue-chart.js
+++ app/ui/dashboard/revenue-chart.js
@@ -2,15 +2,18 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data'
 
-export default function RevenueChart({ revenue }) {
+
+export default async function RevenueChart() { // Jadikan komponen async, hapus props
+  const revenue = await fetchRevenue(); // Ambil data di dalam komponen
+
   const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
-
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
